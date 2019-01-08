f3256a3cd4aff74b9e3d897a8017c36dea1573d9
MS-SD <46464884+MS-SD@users.noreply.github.com>
2019-01-07 16:38:20 -0800

    Add files via upload

---
+++ Connect-ToOffice365.ps1
@@ -0,0 +1,66 @@
+﻿Function Connect-ToOffice365([switch] $UpdateMsOnlineModule) {
+    $ModuleCheck = Get-Module -ListAvailable | Where-Object {$_.Name -eq "msonline"}
+
+    If ($UpdateMsOnlineModule) {
+        $InstalledVer = $ModuleCheck.Version.ToString()
+        $CurrentVer = (Find-Module -Name MsOnline).Version.ToString()
+
+        If ($CurrentVer -gt $InstalledVer) {
+            Write-Host "[ACTION]: Installing newest version of MsOnline Module"
+            Start-Process powershell -Verb runAs -ArgumentList "-command &{Update-Module MSonline -Force}" -Wait
+        }
+        Else {
+            Write-Host "[ALERT]: MsOnline module version is up to date." -f DarkYellow
+        }
+    }
+    Else {
+        Write-Verbose "Bypassing Update"
+    }
+    If (-not($ModuleCheck)) {
+        $admin = [bool](([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match "S-1-5-32-544")
+        If ($admin -eq "True") {
+            Write-Host "[ACTION]: Missing MsOnline Module...Starting installation." -f DarkCyan
+            Install-Module MSOnline
+            Connect-MsolService
+        }
+        else {
+            Write-Host "[ACTION]: Missing MsOnline Module...Starting installation." -f DarkCyan
+            Write-Host "[INFO]: Elevating Powershell to install module"
+            Start-Process powershell -Verb runAs -ArgumentList "-command &{Install-Module MSonline -Force}" -Wait
+            Connect-MsolService
+        }
+    } #If $ModuleCheck
+    Else {
+        If (-Not($domainCheck = Get-MsolDomain -ErrorAction SilentlyContinue)) {
+
+            #Asks for credentials from Operator
+            $UserCredential = Get-Credential -Message "Enter Office 365 tenant credentials"
+
+            #Connects to Office 365 Admin Portal
+            Write-host "[ACTION]:Connecting to Office 365" -f DarkCyan
+            $pass = $false
+
+            If ($UserCredential) {
+                Connect-MsolService -Credential $UserCredential -ErrorAction Stop
+
+                #Stores domain name in variable for output on next line
+                $domain = Get-MsolDomain | Select -ExpandProperty Name
+
+                #Makes connection to Exchange Online using credentials provided above
+                Write-Host "[ACTION]:Connecting to Exchange online. Partner: $domain" -f DarkCyan
+                $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
+                Import-Module -Global (Import-PSSession $Session -DisableNameChecking)
+                $pass = $true
+            }# If
+            Else {
+                Write-Host "[ALERT]:Credentials were incorrect. Please run again." -f DarkYellow
+                Break
+            }# Else
+
+        }# If Get-MSoldomain
+        Else {
+            Write-Host "[ALERT]: You are already connected to $($domainCheck.Name[0]). Please disconnect by closing and re-opening Powershell." -f DarkYellow
+        }
+
+    }# Else
+}# Function Connect-ToOffice365
---
+++ Get-StaleOffice365Accounts.ps1
@@ -0,0 +1,32 @@
+﻿if (!(Get-Command -Name Get-Msoldomain)) {
+    "Please connect to Office 365 via Powershell and try again."; break
+}
+
+$mailbox = Get-Mailbox -ResultSize unlimited  | Where {$_.RecipientTypeDetails -eq "UserMailbox"}
+$count = $mailbox.Count
+$company = (Get-MsolCompanyInformation).DisplayName
+$result = @()
+
+$i = 0
+Foreach ($m in $mailbox) {
+    $obj = Get-MailboxStatistics $m.UserPrincipalName -ErrorAction silentlyContinue |
+        Where {$_.LastLogonTime -le (Get-Date).AddMonths(-1)} |
+        Select-Object @{E = {"$company"}; L = "Company"}, `
+    @{E = {$_.DisplayName}; L = "Account Name"}, `
+    @{E = {$_.LastLogontime.Date.ToShortDateString()}; L = "Last Logon Date"}, `
+    @{E = {$_.LastLogontime.ToShortTimeString()}; L = "Last Logon Time"}
+
+    $result += $obj
+    $I = $i + 1
+    If ($obj) {
+        $activity = $obj | Out-String
+        Write-Progress -Id 2 -Activity "Stale user located: $activity" -Status "Mailbox $i/$count"  -PercentComplete ($i / $mailbox.Count * 100)
+    }# If
+    Else {
+        Write-Progress -Id 2 -Activity "Locating stale users in $($t.client) tenant..." -Status "Mailbox $i/$count" -PercentComplete ($i / $mailbox.Count * 100)
+    }# else
+
+}# Foreach
+
+$result | Export-Csv "$env:USERPROFILE\Desktop\$company StaleUsers.csv"
+Write-Host "File has been created" -f DarkCyan
